ecf184b440cf9552a9c3c06286bfae7706a5c793
Fox <fox.bolduc@ttu.edu>
2018-01-30 12:00:32 -0600

    Navigation buttons and project toggles working

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function showSchool(){
     var showDiv = document.getElementById("schoolProjects");
+    var showButton = document.getElementById("schoolButton");
     var hideDiv = document.getElementById("personalProjects");
+    var hideButton = document.getElementById("personalButton");
+    showButton.style.color = "rgb(117,172, 243)";
+    hideButton.style.color = "#505050";
     showDiv.style.display = "block";
     hideDiv.style.display = "none";
 }
 
 function showPersonal(){
     var showDiv = document.getElementById("personalProjects");
+    var showButton= document.getElementById("personalButton");
     var hideDiv = document.getElementById("schoolProjects");
+    var hideButton = document.getElementById("schoolButton");
+    showButton.style.color = "rgb(117,172, 243)";
+    hideButton.style.color = "#505050";
     showDiv.style.display = "block";
     hideDiv.style.display = "none";
 }
